29a7e5553015afb04de1912dea986b6d74fdfbe0
Rushi <rushikeshshelar.cs@gmail.com>
2023-12-28 23:53:26 +0530

    Utility file for Defining Models

--- types.ts
+++ types.ts
@@ -1,6 +1,6 @@
 export interface Billboard{
     id: string;
-    name: string;
+    label: string;
     imageUrl: string;
 }
 
@@ -10,3 +10,30 @@ export interface Category{
     billboards: Billboard;
 }
 
+export interface Product{
+    id: string;
+    category: Category;
+    name: string;
+    price: string;
+    isFeatured: boolean;
+    size: Size;
+    color: Color;
+    images: Image[];
+}
+
+export interface Size{
+    id: string;
+    name: string;
+    value: string;
+}
+
+export interface Color{
+    id: string;
+    name: string;
+    value: string;
+}
+
+export interface Image{
+    id: string;
+    url: string;
+}
