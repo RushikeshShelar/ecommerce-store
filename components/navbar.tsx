41507c57ad1c435b079b4c8306ad3e48bc5293a5
Rushi <rushikeshshelar.cs@gmail.com>
2023-12-28 23:52:54 +0530

    Created Necessary Components

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,25 @@
-const Navbar = () => {
+import Container from "@/components/ui/container";
+import Link from "next/link";
+import MainNav from "@/components/main-nav";
+import getCategrories from "@/actions/get-categrories";
+import NavbarActions from "./navbar-actions";
+
+export const revalidate = 0;     //Hard Refresh NOt Working
+
+const Navbar = async () => {
+    const categories = await getCategrories()
     return (
-        <nav className="border-b">
-            NAvbar
-        </nav>
+        <div className="border-b">
+            <Container>
+                <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+                    <Link href='/' className="ml-4 flex lg:ml-0 gap-x-2">
+                        <p className="font-bold text-xl">STORE</p>
+                    </Link>
+                    <MainNav data={categories} />
+                    <NavbarActions />
+                </div>
+            </Container>
+        </div>
     )
 }
 
