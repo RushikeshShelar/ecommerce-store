ba96e82312b142e557ff389cda67c15084aa385c
Rushi <rushikeshshelar.cs@gmail.com>
2024-01-03 01:06:34 +0530

    Added Preview Modal to product

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next'
 import { Urbanist } from 'next/font/google'
-import './globals.css'
+
 import Footer from '@/components/footer'
 import Navbar from '@/components/navbar'
+import ModalProvider from '@/providers/modal-provider'
+
+import './globals.css'
 
 const urbanist = Urbanist({ subsets: ['latin'] })
 
@@ -19,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={urbanist.className}>
+        <ModalProvider />
         <Navbar />
         {children}
         <Footer />
--- components/ui/poduct-card.tsx
+++ components/ui/poduct-card.tsx
@@ -3,10 +3,12 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Expand, ShoppingCart } from "lucide-react";
+import { MouseEventHandler } from "react";
 
 import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 interface ProductCardProps {
     data: Product;
@@ -16,11 +18,18 @@ const ProductCard: React.FC<ProductCardProps> = ({
     data
 }) => {
 
+    const previewModal = usePreviewModal();
     const router = useRouter()
 
     const handleClick = () => {
         ;
         router.push(`/product/${data?.id}`)
+    }
+
+    const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation();
+
+        previewModal.onOpen(data);
     }
 
     return (
@@ -38,7 +47,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
                 <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                     <div className="flex gap-x-6 justify-center">
                         <IconButton
-                            onClick={() => { }}
+                            onClick={onPreview}
                             icon={<Expand size={20} className="text-gray-600" />}
                         />
                         <IconButton
