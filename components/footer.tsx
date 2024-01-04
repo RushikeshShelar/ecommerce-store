548217ab8d512cb887d1eaa7e313f63f68f4d191
Rushi <rushikeshshelar.cs@gmail.com>
2024-01-05 00:42:50 +0530

    Changed Store Name and added Footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
+
 const Footer = () => {
-    return(
-        <footer className="bg-whhite border-t">
-            <div className="mx-auto py-18">
+    return (
+        <footer className="bg-whhite border-t px-20 h-12 w-full">
+            <div className="mx-auto py-18 flex justify-between items-center  h-12 w-full ">
                 <p className="text-center text-sm text-black">
-                    &copy; 2023 RushiStore, Inc. All rights reserved.
+                    &copy; 2024 <Link href='/'>NextMart</Link>, Inc. All rights reserved.
+                </p>
+                <p className="text-center text-sm text-black">
+                    Made By <Link href="https://rushikeshshelar.tech">
+                                <span  className="text-gray-600 font-bold">Rushikesh Shelar</span>
+                            </Link>
                 </p>
             </div>
         </footer>
